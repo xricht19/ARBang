b2f882f7f9554fe8ed11ecab2cc425cfa1f8b725
xricht19 <xricht19@stud.fit.vutbr.cz>
2018-02-27 22:18:04 +0100

    First commit with files.

---
+++ Assets/scripts/CameraControl.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+using System;
+
+public class CameraControl : MonoBehaviour
+{
+    public static CameraControl cameraControl;
+
+    public Text heightText;
+    public Text widthText;
+
+
+    [DllImport("ImageProcessingForARCardGames")]
+    static public extern IntPtr CreateImageDetectionAccessPoint();
+    [DllImport("ImageProcessingForARCardGames")]
+    static public extern void DestroyImageDetectionAccessPoint(IntPtr pImageDetectionAccessPoint);
+    [DllImport("ImageProcessingForARCardGames")]
+    static public extern void InitImageDetectionAccessPointCaller(IntPtr pImageDetectionAccessPoint, ref ushort errorCode, ref ushort cameraID);
+    [DllImport("ImageProcessingForARCardGames")]
+    static public extern void InitImageDetectionAccessPointROSCaller(IntPtr pImageDetectionAccessPoint, ref ushort errorCode, int ipAdress, ref ushort port);
+    [DllImport("ImageProcessingForARCardGames")]
+    static public extern void GetVideoResolutionCaller(IntPtr pImageDetectionAccessPoint, ref ushort errorCode, ref ushort width, ref ushort height);
+    [DllImport("ImageProcessingForARCardGames")]
+    static public extern void GetNumberOfAllAvailableDevicesCaller(IntPtr pImageDetectionAccessPoint, ref ushort errorCode, ref ushort numberOfAvailDevices);
+    [DllImport("ImageProcessingForARCardGames")]
+    static public extern void PrepareNextFrameCaller(IntPtr pImageDetectionAccessPoint, ref ushort errorCode);
+    [DllImport("ImageProcessingForARCardGames")]
+    static public extern void GetCurrentFrameDataCaller(IntPtr pImageDetectionAccessPoint, ref ushort errorCode, ref ushort cols, ref ushort rows, ref ushort channels, ref byte[] dataByte);
+    [DllImport("ImageProcessingForARCardGames")]
+    static public extern void IsPlayerActiveByIDCaller(IntPtr pImageDetectionAccessPoint, ref ushort errorCode, ref ushort playerID);
+    [DllImport("ImageProcessingForARCardGames")]
+    static public extern void HasGameObjectChangedCaller(IntPtr pImageDetectionAccessPoint, ref ushort errorCode, ref ushort positionID, ref ushort objectID);
+
+
+    IntPtr pImageDetectionAccessPoint = IntPtr.Zero;
+    private ushort height = 0;
+    private ushort width = 0;
+    private ushort errorCode = 0;
+    private ushort cameraId = 0;
+
+    // object should exist during whole time of application
+    void Awake()
+    {
+        if (cameraControl == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            cameraControl = this;
+        }
+        else if (cameraControl != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // free memory if taken
+    public void OnDestroy()
+    {
+        if (pImageDetectionAccessPoint != IntPtr.Zero)
+        {
+            Debug.Log("Destroing");
+            DestroyImageDetectionAccessPoint(pImageDetectionAccessPoint);
+            pImageDetectionAccessPoint = IntPtr.Zero;
+        }
+    }
+
+
+    // temporary test function
+    public void StartCapture()
+    {
+        if (pImageDetectionAccessPoint == IntPtr.Zero)
+        {
+            Debug.Log("Trying create class");
+            unsafe
+            {
+                pImageDetectionAccessPoint = CreateImageDetectionAccessPoint();
+                InitImageDetectionAccessPointCaller(pImageDetectionAccessPoint, ref errorCode, ref cameraId);
+            }
+            
+        }
+
+        Debug.Log("Test started!");  
+
+        print("Returned camera Id:" + cameraId);
+
+        unsafe
+        {
+            GetVideoResolutionCaller(pImageDetectionAccessPoint, ref errorCode, ref height, ref width);
+            Debug.Log("ErrorCode: "+errorCode);
+            Debug.Log("Width: "+width);
+            Debug.Log("Height: "+height);
+        }
+        heightText.text = height.ToString();
+        widthText.text = width.ToString();
+
+    }
+
+}
---
+++ Assets/scripts/GameControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl gameControl;
+
+    // variable keeped for all scenes
+    public ushort cameraId;
+
+    // name of camera - cannot be serialized
+    public string cameraName;
+
+	// check if the GameControl already exists, create it otherwise
+	void Awake () {
+		if (gameControl == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gameControl = this;
+        }
+        else if (gameControl != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    // saves data setting to file
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        // open file in Unity persistent data path
+        FileStream file = File.Create(Application.persistentDataPath + "/gameDataSetting.dat");
+
+        GameDataSettings data = new GameDataSettings();
+        data.cameraId = cameraId;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    // load data settings from file
+    public void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/gameDataSetting.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gameDataSetting.dat", FileMode.Open);
+            GameDataSettings data = (GameDataSettings)bf.Deserialize(file);
+
+            file.Close();
+
+            cameraId = data.cameraId;
+        }
+    }
+	
+}
+
+[Serializable]
+class GameDataSettings
+{
+    public ushort cameraId;
+}
